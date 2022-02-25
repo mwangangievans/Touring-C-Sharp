a190721b3351463cf3263dfafbfa8e404c1e8ec4
ombuna <ombunaw@gmail.com>
2022-02-25 15:15:23 +0300

    deligate multicust

---
+++ DELEGATES/Demo3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Demo3
+{
+    public delegate void SampleDeligate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleDeligate del = new SampleDeligate(SampleMethodOne);
+            del += SampleMethodTwo;
+            del += SampleMethodThree;
+            del();
+        }
+        public static void SampleMethodOne()
+        {
+            Console.WriteLine("SampleMethodOne invoked");
+        }
+        public static void SampleMethodTwo()
+        {
+            Console.WriteLine("SampleMethodTwo invoked");
+        }
+        public static void SampleMethodThree()
+        {
+            Console.WriteLine("SampleMethodThree invoked");
+        }
+
+    }
+   
+}
