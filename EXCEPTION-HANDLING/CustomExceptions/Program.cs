c5693a80abf232d457c978de1a45d6b87c4ea1d7
ombuna <ombunaw@gmail.com>
2022-02-26 09:15:31 +0300

    customErrorMessages

---
+++ EXCEPTION-HANDLING/CustomExceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CustomExceptions
+{
+    class Program
+    {
+        //creating a custom class and inheriting base class Excemption
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new UserAlreadyLoggedInException("user is logged in  -  no duplicate sessions");
+            }
+            catch (UserAlreadyLoggedInException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        [Serializable]
+        public class UserAlreadyLoggedInException : Exception
+        {
+            public UserAlreadyLoggedInException(string message) : base()
+            {
+            }
+            public UserAlreadyLoggedInException(string message ,Exception innerExcemption) : base(message ,innerExcemption)
+            {
+            }
+            public UserAlreadyLoggedInException(SerializationInfo info, StreamingContext context) : base()
+            {
+            }
+        }
+    }
+}
